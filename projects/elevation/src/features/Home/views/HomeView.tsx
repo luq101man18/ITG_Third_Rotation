38e7d7fb69b0252054c91fb1ff2d28210c28bc78
luq101man18 <luqmanalhasani8@gmail.com>
2024-10-13 14:59:55 -0400

    created product card component

--- projects/elevation/src/features/Home/views/HomeView.tsx
+++ projects/elevation/src/features/Home/views/HomeView.tsx
@@ -1,19 +1,52 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import { useAppSelector } from "../../../hooks/hooks";
 import { RootState } from "../../../store/store";
-
-
+import { PaperProvider } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ProductCard } from "../components/card/Card";
+import { useEffect, useState } from "react";
+import fetchProductsData from "../server/api";
+import { Alert } from "react-native";
+import { styles } from "../styles";
 export default function HomeView() {
 
-    const accessTokenRedux = useAppSelector((state : RootState) => {return state.authentication.accessToken;});
-    const refreshTokenRedux = useAppSelector((state: RootState) => {return state.authentication.refreshToken; });
+    // const accessTokenRedux = useAppSelector((state : RootState) => {return state.authentication.accessToken;});
+    // const refreshTokenRedux = useAppSelector((state: RootState) => {return state.authentication.refreshToken; });
+
+    const [fetchedProducts, setFetchedProducts] = useState([]);
+
+    useEffect(() => {
+        try {
+            const fetchProducts = async () => {
+                const fetchedProductsFromApi = await fetchProductsData();
+                if (fetchedProductsFromApi) {
+                    setFetchedProducts(fetchedProductsFromApi.products);
+                } else {
+                    Alert.alert("product wasn't set yet");
+                }
+            };
+            fetchProducts();
+        } catch (error) {
+            Alert.alert("there is an error");
+        }
+    }, []);
 
     return(
-        <View>
-            <Text> Home View </Text>
-            <Text>{accessTokenRedux}</Text>
-            <Text>{refreshTokenRedux}</Text>
-        </View>
+        <PaperProvider>
+            <SafeAreaView style={{flex: 1}}>
+                <FlatList
+                    style={styles.container}
+                    numColumns={2}
+                    data={fetchedProducts}
+                    renderItem={({item}) => {return(
+                        <View style={styles.product}>
+                            <ProductCard product={item} />
+                        </View>
+                    );} }
+                    keyExtractor={(item) => item.id}
+                />
+            </SafeAreaView>
+        </PaperProvider>
     );
 }
