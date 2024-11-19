3f05ac539c9f250f8bc6ad4a1b3aa5751715ea16
Luqman Al Hasni <67410294+luq101man18@users.noreply.github.com>
2024-11-19 14:37:21 -0500

    modified api functions to use limit and skip and modified flatlist to use onEndReached (#5)

--- projects/elevation/src/navigation/ElevateNav.tsx
+++ projects/elevation/src/navigation/ElevateNav.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { PaperProvider } from 'react-native-paper';
-import Home from '../features/Home/Home';
 import LoginView from '../features/Login/views/LoginView';
 import RegistrationView from '../features/Registration/views/RegistrationView';
 import HomeView from '../features/Home/views/HomeView';
