0c70139981c709ca496a42a8c2f3268e6af04638
luq101man18 <luqmanalhasani8@gmail.com>
2024-11-19 16:48:38 -0500

    home header fixed styling

--- projects/elevation/src/features/Home/components/header/styles.ts
+++ projects/elevation/src/features/Home/components/header/styles.ts
@@ -4,7 +4,7 @@ import { StyleSheet } from 'react-native';
 export const styles =  StyleSheet.create({
     container: {
         marginHorizontal: 30,
-        marginTop: 5,
+        marginTop: 20,
         marginBottom: 10,
     },
     header: {
